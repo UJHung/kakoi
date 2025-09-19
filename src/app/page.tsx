5efbdaf6e1e0a625d9e17ab84a3da9d856aaa6b4
Yu Jie Hung <ujhong636@gmail.com>
2025-09-20 03:15:55 +0800

    feat(views): replace logo and background SVGs with new designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,33 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <div
+      className="relative grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20"
+      style={{
+        backgroundImage: "url('/logo-bg.svg')",
+        backgroundPosition: "left top",
+        backgroundSize: "100px",
+      }}
+    >
       <main className="row-start-2 flex flex-col items-center">
         <Image
           className="dark:invert"
-          src="/logo.svg"
+          src="/logo-vertical.svg"
           alt="Kaku logo"
-          width={100}
-          height={100}
+          width={200}
+          height={200}
           priority
         />
-        <div className="mt-2 text-xl font-bold">KAKOi</div>
-        <div className="mt-3 mb-6">卡片都在這，優惠不會漏</div>
+        <div className="mb-6 text-center text-lg font-semibold">
+          卡在KAKOi，優惠都可以
+        </div>
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <button
             onClick={() => {
               router.push("/cards");
             }}
-            className="bg-foreground text-background hover:bg-foreground/70 flex cursor-pointer items-center justify-center rounded-full px-4 py-3 text-sm font-medium transition-colors disabled:opacity-20 sm:px-5 sm:text-base"
+            className="bg-foreground text-background flex cursor-pointer items-center justify-center rounded-lg px-4 py-3 text-sm font-medium transition-colors hover:bg-gray-800 disabled:opacity-20 sm:px-5 sm:text-base"
           >
-            {/* 即將推出 */}
             立即體驗
           </button>
         </div>
