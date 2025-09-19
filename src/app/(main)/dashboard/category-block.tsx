d4d10e2c052dfab1524ee23b32a55bafd79da54b
Yu Jie Hung <ujhong636@gmail.com>
2025-09-20 02:00:53 +0800

    feat(category-block): add new category icons and update styles for improved UI

--- src/app/(main)/dashboard/category-block.tsx
+++ src/app/(main)/dashboard/category-block.tsx
@@ -3,6 +3,10 @@
 import { FaPlane, FaStore } from "react-icons/fa";
 import { RiShoppingBasketFill } from "react-icons/ri";
 import { TbBowlSpoonFilled } from "react-icons/tb";
+import { FaMotorcycle } from "react-icons/fa6";
+import { MdStore } from "react-icons/md";
+import { BiSolidWallet, BiSolidTrain, BiSolidVideos } from "react-icons/bi";
+
 import { useRouter } from "next/navigation";
 
 export default function CategoryBlock({
@@ -17,20 +21,40 @@ export default function CategoryBlock({
   const router = useRouter();
   const categoryIcon =
     category == "ecommerce" ? (
-      <div className="mb-2 inline-block rounded-md bg-orange-500 p-2">
+      <div className="mb-2 inline-block rounded-md bg-orange-400 p-2">
         <RiShoppingBasketFill size="28" className="text-white" />
       </div>
     ) : category == "travel" ? (
-      <div className="mb-2 inline-block rounded-md bg-yellow-500 p-2">
+      <div className="mb-2 inline-block rounded-md bg-amber-400 p-2">
         <FaPlane size="28" className="text-white" />
       </div>
     ) : category == "convenience" ? (
-      <div className="mb-2 inline-block rounded-md bg-sky-500 p-2">
-        <FaStore size="28" className="text-white" />
+      <div className="mb-2 inline-block rounded-md bg-yellow-400 p-2">
+        <MdStore size="28" className="text-white" />
       </div>
     ) : category == "dining" ? (
-      <div className="mb-2 inline-block rounded-md bg-blue-500 p-2">
+      <div className="mb-2 inline-block rounded-md bg-lime-400 p-2">
         <TbBowlSpoonFilled size="28" className="text-white" />
+      </div>
+    ) : category == "food_delivery" ? (
+      <div className="mb-2 inline-block rounded-md bg-green-500/70 p-2">
+        <FaMotorcycle size="28" className="text-white" />
+      </div>
+    ) : category == "mobile_wallet" ? (
+      <div className="mb-2 inline-block rounded-md bg-teal-500 p-2">
+        <BiSolidWallet size="28" className="text-white" />
+      </div>
+    ) : category == "transport" ? (
+      <div className="mb-2 inline-block rounded-md bg-sky-300 p-2">
+        <BiSolidTrain size="28" className="text-white" />
+      </div>
+    ) : category == "ott" ? (
+      <div className="mb-2 inline-block rounded-md bg-blue-400 p-2">
+        <BiSolidVideos size="28" className="text-white" />
+      </div>
+    ) : category == "grocery_hyper" ? (
+      <div className="mb-2 inline-block rounded-md bg-indigo-400 p-2">
+        <FaStore size="28" className="text-white" />
       </div>
     ) : null;
 
@@ -43,11 +67,11 @@ export default function CategoryBlock({
   return (
     <div
       onClick={() => clickFunction()}
-      className="cursor-pointer rounded-lg bg-white p-5 transition-shadow hover:md:shadow-md"
+      className="cursor-pointer rounded-lg bg-white p-4 transition-shadow sm:p-5 hover:md:shadow-md"
     >
       {categoryIcon}
-      <h3 className="mb-1 text-lg font-semibold">{title}</h3>
-      <p className="text-xs text-gray-400">{desc}</p>
+      <h3 className="text-md font-semibold sm:text-lg">{title}</h3>
+      <p className="mt-1 hidden text-xs text-gray-400 md:block">{desc}</p>
     </div>
   );
 }
--- src/data/dashboard.ts
+++ src/data/dashboard.ts
@@ -1,7 +1,7 @@
 export const categories = [
   {
     category: "ecommerce",
-    title: "網購",
+    title: "電商",
     desc: "蝦皮｜momo｜PChome｜酷澎 ...",
   },
   {
@@ -19,28 +19,46 @@ export const categories = [
     title: "餐飲",
     desc: "全台餐飲｜酒店｜飯店 ...",
   },
+  {
+    category: "food_delivery",
+    title: "外送",
+    desc: "UberEats｜foodpanda",
+  },
+  {
+    category: "mobile_wallet",
+    title: "行動支付",
+    desc: "Line Pay｜街口支付",
+  },
+  {
+    category: "transport",
+    title: "交通",
+    desc: "高鐵｜台鐵｜計程車 ...",
+  },
+  {
+    category: "ott",
+    title: "串流平台",
+    desc: "Netflix｜Disney+｜Spotify ...",
+  },
+  {
+    category: "grocery_hyper",
+    title: "量販超市",
+    desc: "家樂福｜大潤發｜全聯 ...",
+  },
 ];
 
 export const searchTags = [
-  // Convenience stores
   "7-11",
   "全家",
-
-  // E-commerce
   "蝦皮",
+  "全聯",
   "momo",
   "PChome",
-
-  // Transportation
+  "行動支付",
   "Uber",
   "高鐵",
-
-  // Travel/Booking
-  "Agoda",
-  "Booking.com",
-  "Klook",
-
-  // Dining/Delivery
-  "UberEats",
-  "foodpanda",
+  "餐飲",
+  "百貨",
+  "服飾",
+  "海外",
+  "日本",
 ];
